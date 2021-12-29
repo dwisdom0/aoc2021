c838299ffa5983b173b6d61bb4ca5dc0380c95af
David Wisdom <dwisdom0@gmail.com>
2021-12-29 17:25:07 -0500

    rename folders

--- day5/test_2.py
+++
@@ -1,9 +0,0 @@
-from day5_2 import build_board, apply_lines
-
-
-def test_top_right_bottom_left():
-  lines = [((10,5),(5,10))]
-  board = build_board(lines)
-  board = apply_lines(board, lines)
-  print(board)
-
--- day7/get_input.sh
+++
@@ -1,1 +0,0 @@
-curl -b ../my_cookie.tsv https://adventofcode.com/2021/day/7/input > input.txt
